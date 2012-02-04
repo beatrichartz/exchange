e9265947de25513ddb7ea5146235ec0281dca502
beatrichartz <beat.richartz@gmail.com>
2012-02-04 20:02:20 +0100

    . fully specced file cache class

--- spec/exchange/cache/file_spec.rb
+++ spec/exchange/cache/file_spec.rb
@@ -27,7 +27,7 @@ describe "Exchange::Cache::File" do
       end
       context "with an monthly cache" do
         before(:each) do
-          subject.should_receive(:key).with('API_CLASS', :monthly).and_return('KEY')
+          subject.should_receive(:key).with('API_CLASS', an_instance_of(Symbol)).and_return('KEY')
           Exchange::Configuration.should_receive(:filestore_path).and_return('STORE')
           ::File.should_receive(:exists?).with('STORE/KEY').and_return(true)
           ::File.should_receive(:read).with('STORE/KEY').and_return 'CONTENT'
@@ -37,15 +37,30 @@ describe "Exchange::Cache::File" do
         end
       end
     end
-    context "when no result is returned" do
+    context "when no file is cached yet" do
       before(:each) do
-        subject.should_receive(:key).with('API_CLASS', :monthly).and_return('KEY')
+        subject.should_receive(:key).with('API_CLASS', an_instance_of(Symbol)).at_most(3).times.and_return('KEY')
         Exchange::Configuration.should_receive(:filestore_path).and_return('STORE')
-        ::File.should_receive(:exists?).with('STORE/KEY').and_return(true)
-        ::File.should_receive(:read).with('STORE/KEY').and_return 'CONTENT'
+        ::File.should_receive(:exists?).with('STORE/KEY').and_return(false)
+        Dir.should_receive(:entries).with('STORE').once.and_return(%W(entry entry2))
+        ::File.should_receive(:delete).with(an_instance_of(String)).twice
+        FileUtils.should_receive(:mkdir_p).once
+        ::File.should_receive(:open).once
       end
       it "should return the file contents" do
-        subject.cached('API_CLASS', :cache_period => :monthly) { 'something' }.should == 'CONTENT'
+        subject.cached('API_CLASS', :cache_period => :monthly) { 'RESULT' }.should == 'RESULT'
+      end
+    end
+    context "when no result is returned" do
+      before(:each) do
+        subject.should_receive(:key).with('API_CLASS', an_instance_of(Symbol)).at_most(3).times.and_return('KEY')
+        Exchange::Configuration.should_receive(:filestore_path).and_return('STORE')
+        ::File.should_receive(:exists?).with('STORE/KEY').and_return(false)
+        FileUtils.should_receive(:mkdir_p).never
+        ::File.should_receive(:open).never
+      end
+      it "should return the file contents" do
+        subject.cached('API_CLASS', :cache_period => :monthly) { '' }.should == ''
       end
     end
   end
