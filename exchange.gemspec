6513b7553b8da43a29892d4f45111fbd41f17370
beatrichartz <beat.richartz@gmail.com>
2012-10-09 23:07:37 +0200

    . readd json as an explicit dependency

--- exchange.gemspec
+++ exchange.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |s|
   s.executables       = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths     = ["lib"]
   
+  s.add_dependency             "json", ">= 1.0.0"
   s.add_development_dependency "yard", ">= 0.7.4"
   s.add_development_dependency "bundler", ">= 1.0.0"
 end
--- lib/exchange/external_api/json.rb
+++ lib/exchange/external_api/json.rb
@@ -1,22 +1,12 @@
 module Exchange
   module ExternalAPI
     
-    # The json base class takes care of JSON apis. It assumes you would want to use json as a parser and preloads the gem
-    # Also, this may serve as a base for some operations which might be common to the json apis
+    # The json base class takes care of JSON apis. 
+    # This may serve as a base for some operations which might be common to the json apis
     # @author Beat Richartz
     # @version 0.6
     # @since 0.6
     #
-    class JSON < Base
-      
-      # Initializer, essentially takes the arguments passed to initialization, loads the json gem on the way
-      # and passes the arguments to the api base
-      #
-      def initialize *args
-        Exchange::GemLoader.new('json').try_load unless defined?(::JSON)
-        super *args
-      end
-      
-    end
+    JSON = Class.new Base
   end
 end
