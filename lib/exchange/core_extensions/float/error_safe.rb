4fe48f51a3f0bf218cdcbb32005ae5a5df297bb2
beatrichartz <beat.richartz@gmail.com>
2012-10-30 22:34:19 +0100

    . beautify the error safe module

--- lib/exchange/core_extensions/float/error_safe.rb
+++ lib/exchange/core_extensions/float/error_safe.rb
@@ -4,17 +4,36 @@ module Exchange
   #
   module ErrorSafe
     
+    # Installs a method chain that overwrites the old error prone meth with the new one
+    #
+    def self.money_error_preventing_method_chain base, meth
+      base.send :alias_method, :"#{meth}with_errors", meth
+      base.send :alias_method, meth, :"#{meth}without_errors"
+    end
+    
+    # @!macro prevent_errors_with_exchange_for
+    #   Prevents float errors when dealing with instances of Exchange::Money
+    #   By Typecasting the float into a Big Decimal
+    #   @method $1(other)
+    #
+    def self.prevent_errors_with_exchange_for base, meth
+      base.send(:define_method, :"#{meth}without_errors", lambda { |other|
+        if other.is_a?(Exchange::Money)
+          BigDecimal.new(self.to_s).send(meth, other).to_f
+        else
+          send(:"#{meth}with_errors", other)
+        end
+      })
+      money_error_preventing_method_chain base, meth
+    end
+    
     def self.included base
       %W(* / + -).each do |meth|
-        base.send(:define_method, :"#{meth}without_errors", lambda { |other|
-          if other.is_a?(Exchange::Money)
-            (BigDecimal.new(self.to_s).send(meth, other)).to_f
-          else
-            send(:"#{meth}with_errors", other)
-          end
-        })
-        base.send :alias_method, :"#{meth}with_errors", meth.to_sym
-        base.send :alias_method, meth.to_sym, :"#{meth}without_errors"
+        
+        # @macro prevent_errors_with_exchange_for
+        #
+        prevent_errors_with_exchange_for base, meth.to_sym
+        
       end
     end
     
