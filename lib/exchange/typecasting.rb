54792f677459319e6076c6f31d2a420d71f49562
beatrichartz <beat.richartz@gmail.com>
2012-10-30 22:09:01 +0100

    . split typecasting into different methods

--- lib/exchange/typecasting.rb
+++ lib/exchange/typecasting.rb
@@ -45,6 +45,73 @@ module Exchange
   # @version 0.9.0
   #
   module Typecasting
+    
+    # @private
+    # @macro [attach] install_money_getters
+    #
+    def install_money_getter attribute, options={}
+      
+      define_method :"#{attribute}_with_exchange_typecasting" do
+        currency = evaluate_money_option(options[:currency]) if options[:currency]
+        
+        test_for_currency_error(currency)
+        
+        time     = evaluate_money_option(options[:at]) if options[:at]
+        
+        Exchange::Money.new(send(:"#{attribute}_without_exchange_typecasting")) do |c|
+          c.currency = currency
+          c.time     = time if time
+        end
+      end
+      exchange_typecasting_alias_method_chain attribute
+      
+    end
+    
+    # @private
+    # @macro [attach] install_money_setters
+    #
+    def install_money_setter attribute, options={}
+      define_method :"#{attribute}_with_exchange_typecasting=" do |data|
+        att = send(attribute)
+        attribute_setter = :"#{attribute}_without_exchange_typecasting="
+        
+        if !data.respond_to?(:currency)
+          send(attribute_setter, data)
+        elsif att.currency == data.currency
+          send(attribute_setter, data.value)
+        elsif att.currency != data.currency
+          send(attribute_setter, data.send(:"to_#{att.currency}").value)
+        end
+      end
+      exchange_typecasting_alias_method_chain attribute, '='
+    end
+    
+    # Install an alias method chain for an attribute
+    # @param [String, Symbol] attribute The attribute to install the alias method chain for
+    # @param [String] setter The setter sign ('=') if this is a setter
+    #
+    def exchange_typecasting_alias_method_chain attribute, setter=nil
+      alias_method :"#{attribute}_without_exchange_typecasting#{setter}", :"#{attribute}#{setter}"
+      alias_method :"#{attribute}#{setter}", :"#{attribute}_with_exchange_typecasting#{setter}"
+    end
+    
+    # @private
+    # @macro [attach] install_money_option_eval
+    #
+    def install_money_option_eval
+      define_method :evaluate_money_option do |option|
+        option.is_a?(Proc) ? instance_eval(&option) : send(option)
+      end
+    end
+    
+    # @private
+    # @macro [attach] install_currency_error_tester
+    #
+    def install_currency_error_tester
+      define_method :test_for_currency_error do |currency|
+        raise NoCurrencyError.new("No currency is given for typecasting #{attribute}. Make sure a currency is present") unless currency
+      end
+    end
 
     # installs a setter and a getter for the attribute you want to typecast as exchange money
     # @overload def money(*attributes, options={})
@@ -64,39 +131,28 @@ module Exchange
       
       attributes.each do |attribute|
         
-        define_method :"#{attribute}_with_exchange_typecasting" do
-          currency = evaluate_money_option(options[:currency]) if options[:currency]
-          raise NoCurrencyError.new("No currency is given for typecasting #{attributes.join(', ')}. Make sure a currency is present") unless currency
-          
-          time     = evaluate_money_option(options[:at]) if options[:at]
-          
-          Exchange::Money.new(send(:"#{attribute}_without_exchange_typecasting")) do |c|
-            c.currency = currency
-            c.time     = time if time
-          end
-        end
-        alias_method :"#{attribute}_without_exchange_typecasting", attribute.to_sym
-        alias_method attribute.to_sym, :"#{attribute}_with_exchange_typecasting"
-      
-        define_method :"#{attribute}_with_exchange_typecasting=" do |data|
-          att = send(attribute)
-          
-          if !data.respond_to?(:currency)
-            send(:"#{attribute}_without_exchange_typecasting=", data)
-          elsif att.currency == data.currency
-            send(:"#{attribute}_without_exchange_typecasting=", data.value)
-          elsif att.currency != data.currency
-            send(:"#{attribute}_without_exchange_typecasting=", data.send(:"to_#{att.currency}").value)
-          end
-        end
-        alias_method :"#{attribute}_without_exchange_typecasting=", :"#{attribute}="
-        alias_method :"#{attribute}=", :"#{attribute}_with_exchange_typecasting="
+        # Get the attribute typecasted into money 
+        # @return [Exchange::Money] an instance of money
+        #
+        install_money_getter attribute, options
+        
+        # Set the attribute either with money or just any data
+        # Implicitly converts values given that are not in the same currency as the currency option evaluates to
+        # @param [Exchange::Money, String, Numberic] data The data to set the attribute to
+        # 
+        install_money_setter attribute, options
         
       end
       
-      define_method :evaluate_money_option do |option|
-        option.is_a?(Proc) ? instance_eval(&option) : send(option)
-      end
+      # Evaluates options given either as symbols or as procs
+      # @param [Symbol, Proc] option The option to evaluate
+      #
+      install_money_option_eval
+      
+      # Evaluates whether an error should be raised because there is no currency present
+      # @param [Symbol] currency The currency, if given
+      #
+      install_currency_error_tester
     end
         
     # Is raised when no currency is given for typecasting
